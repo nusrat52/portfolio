e3dd1fa1afcc7a27579af2e85b34515a70884a39
Nusrat <nusretibragimli@gmail.com>
2021-04-28 15:51:43 +0400

    first

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
       <App />
-    </Router>,
+    </HashRouter>,
   document.getElementById("root")
 );
